6cf65c62f976efacb3277a164b13fc27955ce39d
Andrew Thompson <andrew@hijacked.us>
2018-06-30 14:19:29 -0700

    Make something that actually compiles and calls a h3 function

---
+++ c_src/h3.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 Helium Systems Inc. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#include "erl_nif.h"
+#include <string.h>
+#include <stdbool.h>
+#include "h3api.h"
+
+static ERL_NIF_TERM
+mk_atom(ErlNifEnv * env, const char * atom)
+{
+    ERL_NIF_TERM ret;
+
+    if (!enif_make_existing_atom(env, atom, &ret, ERL_NIF_LATIN1))
+    {
+        return enif_make_atom(env, atom);
+    }
+
+    return ret;
+}
+
+static ERL_NIF_TERM
+erl_num_hexagons(ErlNifEnv * env, int argc, const ERL_NIF_TERM argv[])
+{
+    int res;
+    if (!enif_get_int(env, argv[0], &res)) {
+        return enif_make_badarg(env);
+    }
+    int64_t result = numHexagons(res);
+    return enif_make_int64(env, result);
+}
+
+static ErlNifFunc nif_funcs[] = {
+    {"num_hexagons", 1, erl_num_hexagons, 0}
+    };
+
+static int
+load(ErlNifEnv * env, void ** priv_data, ERL_NIF_TERM load_info)
+{
+    (void)priv_data;
+    (void)load_info;
+    return 0;
+}
+
+ERL_NIF_INIT(h3, nif_funcs, load, NULL, NULL, NULL);
--- src/h3.erl
+++ src/h3.erl
@@ -1,13 +1,26 @@
 -module(h3).
+-export([num_hexagons/1]).
+-on_load(init/0).
 
-%% API exports
--export([]).
+-define(APPNAME, h3).
+-define(LIBNAME, 'h3').
 
-%%====================================================================
-%% API functions
-%%====================================================================
+num_hexagons(_) ->
+    not_loaded(?LINE).
 
+init() ->
+    SoName = case code:priv_dir(?APPNAME) of
+        {error, bad_name} ->
+            case filelib:is_dir(filename:join(["..", priv])) of
+                true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                _ ->
+                    filename:join([priv, ?LIBNAME])
+            end;
+        Dir ->
+            filename:join(Dir, ?LIBNAME)
+    end,
+    erlang:load_nif(SoName, 0).
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+not_loaded(Line) ->
+    exit({not_loaded, [{module, ?MODULE}, {line, Line}]}).
