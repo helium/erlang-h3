e4436a79cd5c02a7e1595bbae5fa0764308b6a6f
Andrew Thompson <andrew@hijacked.us>
2018-06-30 13:28:08 -0700

    Add some basic stuff

---
+++ c_src/compile.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+VERSION="v3.0.7"
+
+
+if [ ! -d c_src/h3 ]; then
+    git clone https://github.com/uber/h3.git c_src/h3
+fi
+
+cd c_src/h3
+
+CURRENT_VERSION=`git describe --tags`
+
+if [ ! "$VERSION" = "$CURRENT_VERSION" ]; then
+    git clean -ddxxff
+    git fetch
+    git checkout $VERSION
+fi
+
+if [ ! -d build ]; then
+    mkdir build
+fi
+cd build
+if [ ! -f Makefile ]; then
+    cmake ..
+fi
+make -j h3
