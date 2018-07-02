e96e8b4105d0473ea522395360f1890986054f96
Rahul Garg <vihu89@gmail.com>
2018-07-02 10:29:08 -0700

    add k_ring_distance unit tests

--- test/h3_SUITE.erl
+++ test/h3_SUITE.erl
@@ -17,7 +17,9 @@
          hex_area_km2_decreasing_test/1,
          self_not_a_neighbor_test/1,
          h3_of_geo_coord_test/1,
-         k_ring_origin_index_test/1
+         k_ring_origin_index_test/1,
+         k_ring_distance_origin_test/1,
+         k_ring_distance_test/1
         ]).
 
 all() ->
@@ -33,45 +35,56 @@ all() ->
      hex_area_km2_decreasing_test,
      self_not_a_neighbor_test,
      h3_of_geo_coord_test,
-     k_ring_origin_index_test
+     k_ring_origin_index_test,
+     k_ring_distance_origin_test,
+     k_ring_distance_test
     ].
 
 init_per_testcase(_, Config) ->
     Resolutions = lists:seq(0, 15),
-    [{resolutions, Resolutions} | Config].
+    ParisIndex = h3:from_geo({h3:degs_to_rads(37.3615593), h3:degs_to_rads(-122.0553238)}, 7),
+    [{resolutions, Resolutions}, {paris_index, ParisIndex} | Config].
 
 end_per_testcase(_, _Config) ->
     ok.
 
 degs_to_rads_test(_Config) ->
-    0.017453292519943295 = h3:degs_to_rads(1.0).
+    0.017453292519943295 = h3:degs_to_rads(1.0),
+    ok.
 
 rads_to_degs_test(_Config) ->
-    57.29577951308232 = h3:rads_to_degs(1.0).
+    57.29577951308232 = h3:rads_to_degs(1.0),
+    ok.
 
 validity_test(_Config) ->
     H3 = h3:from_string("845ad1bffffffff"),
     596072861467148287 = H3,
-    true = h3:is_valid(H3).
+    true = h3:is_valid(H3),
+    ok.
 
 d2r_r2d_roundtrip_test(_Config) ->
-    40.7128 = h3:rads_to_degs(h3:degs_to_rads(40.7128)).
+    40.7128 = h3:rads_to_degs(h3:degs_to_rads(40.7128)),
+    ok.
 
 r2d_d2r_roundtrip_test(_Config) ->
-    0.7105724077 = h3:degs_to_rads(h3:rads_to_degs(0.7105724077)).
+    0.7105724077 = h3:degs_to_rads(h3:rads_to_degs(0.7105724077)),
+    ok.
 
 max_k_ring_size_test(_Config) ->
-    true = h3:max_k_ring_size(3) > 0.
+    true = h3:max_k_ring_size(3) > 0,
+    ok.
 
 geo_to_h3_test(_Config) ->
     Res = h3:from_geo({h3:degs_to_rads(40.689167), h3:degs_to_rads(-74.044444)}, 5),
-    "852a1073fffffff" = h3:to_string(Res).
+    "852a1073fffffff" = h3:to_string(Res),
+    ok.
 
 h3_to_geo_test(_Config) ->
     {Lat, Long} = h3:to_geo(h3:from_string("845ad1bffffffff")),
     ct:pal("Lat: ~p, Long: ~p", [h3:rads_to_degs(Lat), h3:rads_to_degs(Long)]),
     22.320484717937624 = h3:rads_to_degs(Lat),
-    169.72002399032806 = h3:rads_to_degs(Long).
+    169.72002399032806 = h3:rads_to_degs(Long),
+    ok.
 
 h3_to_geo_boundary_test(_Config) ->
     Paris = h3:from_geo({h3:degs_to_rads(48.8566), h3:degs_to_rads(2.3522)}, 9),
@@ -96,14 +109,31 @@ hex_area_km2_decreasing_test(Config) ->
 
 self_not_a_neighbor_test(_Config) ->
     SFH3 = h3:from_geo({0.659966917655, -2.1364398519396}, 9),
-    false = h3:indices_are_neighbors(SFH3, SFH3).
+    false = h3:indices_are_neighbors(SFH3, SFH3),
+    ok.
 
 h3_of_geo_coord_test(_Config) ->
     Paris = h3:from_geo({h3:degs_to_rads(48.8566), h3:degs_to_rads(2.3522)}, 9),
-    "891fb466257ffff" = h3:to_string(Paris).
+    "891fb466257ffff" = h3:to_string(Paris),
+    ok.
 
-k_ring_origin_index_test(_Config) ->
-    H3Index = h3:from_geo({h3:degs_to_rads(37.3615593), h3:degs_to_rads(-122.0553238)}, 7),
+k_ring_origin_index_test(Config) ->
+    ParisIndex = proplists:get_value(paris_index, Config),
     [OriginIndex] = h3:k_ring(h3:from_geo({h3:degs_to_rads(37.3615593), h3:degs_to_rads(-122.0553238)}, 7), 0),
-    H3Index = OriginIndex,
+    ParisIndex = OriginIndex,
+    ok.
+
+k_ring_distance_origin_test(Config) ->
+    ParisIndex = proplists:get_value(paris_index, Config),
+    [{OriginIndex, 0}] = h3:k_ring_distances(ParisIndex, 0),
+    OriginIndex = ParisIndex,
+    ok.
+
+k_ring_distance_test(Config) ->
+    ParisIndex = proplists:get_value(paris_index, Config),
+    K = 1,
+    %% kring algorithm always returns 7 indices with k=1, in no particular order
+    %% 6 vertices of the hexagon and 1 being the origin index
+    7 = h3:max_k_ring_size(K),
+    7 = length(h3:k_ring_distances(ParisIndex, K)),
     ok.
